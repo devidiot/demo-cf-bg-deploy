4da1ef3b8b90dd1e1e2f66c50ed9cb4b51fb3096
Ryan Moon <ryan@mdpert.com>
2022-12-30 00:55:40 +0000

    first commit

---
+++ src/test/java/com/mdpert/bluegreendeploydemo/BlueGreenDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdpert.bluegreendeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueGreenDeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
