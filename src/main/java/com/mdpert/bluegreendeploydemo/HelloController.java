902684478e1cf83f6fec6a2bd73418bcedd415d1
Ryan Moon <ryan@mdpert.com>
2023-01-02 06:40:13 +0000

    BTP logging configurations

--- src/main/java/com/mdpert/bluegreendeploydemo/HelloController.java
+++ src/main/java/com/mdpert/bluegreendeploydemo/HelloController.java
@@ -1,14 +1,20 @@
 package com.mdpert.bluegreendeploydemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
+
+    private Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/ver")
     public @ResponseBody String version() {
-        return "1.0.4";
+        System.out.println("######################## custom sysout log ################");
+        log.info("######################## custom slf4j log ################");
+        return "1.0.5";
     }
 }
